1959680c399018aba214d4d22498a2da7f0ad651
Unity05 <38768420+Unity05@users.noreply.github.com>
2018-06-27 21:38:17 +0200

    Just the first Upload for this Project
    Version 1.0

---
+++ Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace AppStopper17_0
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            this.stoppAppB = new System.Windows.Forms.Button();
+            this.errorTB = new System.Windows.Forms.RichTextBox();
+            this.stopProcB = new System.Windows.Forms.Button();
+            this.servCB = new System.Windows.Forms.ComboBox();
+            this.procCB = new System.Windows.Forms.ComboBox();
+            this.updateB = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // stoppAppB
+            // 
+            this.stoppAppB.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.stoppAppB.Location = new System.Drawing.Point(100, 45);
+            this.stoppAppB.Name = "stoppAppB";
+            this.stoppAppB.Size = new System.Drawing.Size(95, 48);
+            this.stoppAppB.TabIndex = 1;
+            this.stoppAppB.Text = "stop serv";
+            this.stoppAppB.UseVisualStyleBackColor = false;
+            this.stoppAppB.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // errorTB
+            // 
+            this.errorTB.Location = new System.Drawing.Point(13, 159);
+            this.errorTB.Name = "errorTB";
+            this.errorTB.Size = new System.Drawing.Size(285, 157);
+            this.errorTB.TabIndex = 2;
+            this.errorTB.Text = "";
+            // 
+            // stopProcB
+            // 
+            this.stopProcB.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.stopProcB.Location = new System.Drawing.Point(100, 99);
+            this.stopProcB.Name = "stopProcB";
+            this.stopProcB.Size = new System.Drawing.Size(95, 48);
+            this.stopProcB.TabIndex = 6;
+            this.stopProcB.Text = "stop proc";
+            this.stopProcB.UseVisualStyleBackColor = false;
+            this.stopProcB.Click += new System.EventHandler(this.stopProcB_Click);
+            // 
+            // servCB
+            // 
+            this.servCB.FormattingEnabled = true;
+            this.servCB.Location = new System.Drawing.Point(201, 56);
+            this.servCB.Name = "servCB";
+            this.servCB.Size = new System.Drawing.Size(300, 28);
+            this.servCB.TabIndex = 7;
+            // 
+            // procCB
+            // 
+            this.procCB.FormattingEnabled = true;
+            this.procCB.Location = new System.Drawing.Point(201, 110);
+            this.procCB.Name = "procCB";
+            this.procCB.Size = new System.Drawing.Size(300, 28);
+            this.procCB.TabIndex = 8;
+            // 
+            // updateB
+            // 
+            this.updateB.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.updateB.Location = new System.Drawing.Point(406, 159);
+            this.updateB.Name = "updateB";
+            this.updateB.Size = new System.Drawing.Size(95, 48);
+            this.updateB.TabIndex = 9;
+            this.updateB.Text = "Update";
+            this.updateB.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 419);
+            this.Controls.Add(this.updateB);
+            this.Controls.Add(this.procCB);
+            this.Controls.Add(this.servCB);
+            this.Controls.Add(this.stopProcB);
+            this.Controls.Add(this.errorTB);
+            this.Controls.Add(this.stoppAppB);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "Form1";
+            this.Text = "Stopper";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+        private System.Windows.Forms.Button stoppAppB;
+        private System.Windows.Forms.RichTextBox errorTB;
+        private System.Windows.Forms.Button stopProcB;
+        private System.Windows.Forms.ComboBox procCB;
+        private System.Windows.Forms.ComboBox servCB;
+        private System.Windows.Forms.Button updateB;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ServiceProcess;
+using System.Diagnostics;
+
+namespace AppStopper17_0
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Update();
+        }
+
+        private ServiceController[] services = ServiceController.GetServices();
+        private Process[] procs = Process.GetProcesses();
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string enteredApp = this.servCB.GetItemText(this.servCB.SelectedItem);
+            foreach (ServiceController service in services)
+            {
+                if (service.ServiceName == enteredApp)
+                {
+                    DialogResult result = MessageBox.Show("Do you really want to stop " + enteredApp + "? It could have a negative impact on your system.", "Abfrage", MessageBoxButtons.YesNo);
+                    if (result == DialogResult.Yes)
+                    {
+                        switch (service.Status.ToString())
+                        {
+                            case "Running":
+                                try
+                                {
+                                    service.Stop();
+                                    errorTB.Text = service.ServiceName + " was stopped";
+                                }
+                                catch (Exception ex)
+                                {
+                                    errorTB.Text = "Error: " + service.ServiceName + " cant be stopped || Reason: not enough rights";
+                                }
+                                break;
+                            case "Stopped":
+                                errorTB.Text = "Error: " + service.ServiceName + " can't be stopped || Reason: was already stopped";
+                                break;
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void stopProcB_Click(object sender, EventArgs e)
+        {
+            string enteredProc = this.procCB.GetItemText(this.procCB.SelectedItem);
+            foreach (Process proc in procs)
+            {
+                if (proc.ProcessName == enteredProc)
+                {
+                    DialogResult result = MessageBox.Show("Do you really want to stop " + enteredProc + "? It could have a negative impact on your system.", "Abfrage", MessageBoxButtons.YesNo);
+                    if (result == DialogResult.Yes)
+                    {
+                        proc.Kill();
+                        errorTB.Text = proc.ProcessName + " was killed";
+                    }
+                    break;
+                }
+            }
+        }
+
+        private void Update()
+        {
+            foreach (ServiceController service in services)
+            {
+                if (service.Status == ServiceControllerStatus.Running)
+                {
+                    servCB.Items.Add(service.ServiceName);
+                }
+            }
+            foreach (Process proc in procs)
+            {
+                procCB.Items.Add(proc.ProcessName);
+            }
+        }
+    }
+}
